c4cb994c4279bd279df9e1cc9def7d0ba6fdd475
jungjuleepaygate <superjames19@brandeis.edu>
2022-05-03 23:03:29 +0900

    HTTPS TEST

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ const start = async () => {
 
         await connection.connect()
 
-        app.listen(443, () => {
+        https.createServer(https_cert, app).listen(443, () => {
 
             console.log(`Server is listening on http port ${https_port}`)
 
