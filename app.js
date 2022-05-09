1a4f77b09d6417c2f2540288b68f546bb53108e1
euemmm <superjames19@brandeis.edu>
2022-05-09 07:10:37 +0000

    HTTP TEST

--- app.js
+++ app.js
@@ -2,20 +2,8 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const http = require('http')
-const https = require('https')
-
-const fs = require("fs");
-
-const https_cert = {
-    key: fs.readFileSync("./pem/key.pem"),
-    cert: fs.readFileSync("./pem/cert.pem"),
-}
 
 const connection = require('./db/connect')
-
-const http_port = process.env.HTTP_PORT
-const https_port = process.env.HTTPS_PORT
 
 app.use('/', function (req, res, next) {
     console.log(req.socket.remoteAddress)
@@ -44,9 +32,9 @@ const start = async () => {
 
         await connection.connect()
 
-        https.createServer(https_cert, app).listen(443, () => {
+        app.listen(process.env.PORT, () => {
 
-            console.log(`Server is listening on http port ${https_port}`)
+            console.log("Server Listening On Port 8080")
 
         })
 
